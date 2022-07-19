d5c7bea124f9537334d08f0d382cbf7161ff0707
Andrei <kaptsiuhandrei@gmail.com>
2022-07-19 17:25:51 +0300

    feat: add doc file for app/doc adress

--- src/myDb/myDb.ts
+++ src/myDb/myDb.ts
@@ -4,12 +4,72 @@ import { FavoritesDto } from 'src/favorites/dto/favorites.dto';
 import { TrackDto } from 'src/tracks/dto/track.dto';
 import { UserDto } from 'src/users/dto/user.dto';
 
-export const ALBUMS_DB: AlbumDto[] = [];
-export const ARTISTS_DB: ArtistDto[] = [];
+export const ALBUMS_DB: AlbumDto[] = [
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e288c',
+    name: 'mega alb',
+    year: 1988,
+    artistId: '6b6e261f-3dbe-45ae-9ac4-6f6da02e287b',
+  },
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e288d',
+    name: 'world',
+    year: 2001,
+    artistId: '6b6e261f-3dbe-45ae-9ac4-6f6da02e287b',
+  },
+];
+
+export const ARTISTS_DB: ArtistDto[] = [
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e287b',
+    name: 'Linkin park',
+    grammy: true,
+  },
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e288b',
+    name: 'Ocean',
+    grammy: false,
+  },
+];
+
 export const FAVORITES_DB: FavoritesDto = {
-  artists: [],
-  albums: [],
-  tracks: [],
+  artists: ['6b6e261f-3dbe-45ae-9ac4-6f6da02e288b'],
+  albums: ['6b6e261f-3dbe-45ae-9ac4-6f6da02e288c'],
+  tracks: ['6b6e261f-3dbe-45ae-9ac4-6f6da02e289d'],
 };
-export const TRACKS_DB: TrackDto[] = [];
-export const USERS_DB: UserDto[] = [];
+
+export const TRACKS_DB: TrackDto[] = [
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e289a',
+    name: 'new track',
+    artistId: null,
+    albumId: null,
+    duration: 5,
+  },
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e289d',
+    name: 'mega track',
+    artistId: null,
+    albumId: null,
+    duration: 4,
+  },
+];
+
+export const USERS_DB: UserDto[] = [
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e289c',
+    login: 'Tonni',
+    password: 'secret',
+    version: 1,
+    createdAt: 1657700046,
+    updatedAt: 1657700046,
+  },
+  {
+    id: '6b6e261f-3dbe-45ae-9ac4-6f6da02e289b',
+    login: 'Artur',
+    password: 'world',
+    version: 1,
+    createdAt: 1657700046,
+    updatedAt: 1657700046,
+  },
+];
